08f068bed2a65bcd29ace9a4a7751d4736dd28f1
Felix Hahn <info@hahn-felix.de>
2024-06-15 22:35:38 +0200

    Update file-editor.php

--- views/admin/file-editor.php
+++ views/admin/file-editor.php
@@ -10,8 +10,8 @@ use humhub\libs\Html;
         <h3 style="margin-top:0"><?= Yii::t('ModuleEditorModule.admin', 'File Editor') ?></h3>
     </div>
     <div class="form-group">
-        <p><?= Yii::t('ModuleEditorModul.admin', 'Module: ') . $model->moduleId ?></p>
-        <p><?= Yii::t('ModuleEditorModul.admin', 'File: ') . $model->file ?></p>
+        <p><?= Yii::t('ModuleEditorModule.admin', 'Module: ') . $model->moduleId ?></p>
+        <p><?= Yii::t('ModuleEditorModule.admin', 'File: ') . $model->file ?></p>
         <?= $form->field($model, 'content')->textarea(['rows' => 20]); ?>
     </div>
     <div class="form-group">
