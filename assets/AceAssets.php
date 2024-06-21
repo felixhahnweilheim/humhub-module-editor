e88b55438646ab328eefcea62aa0335cfd5e6bc8
Felix Hahn <info@hahn-felix.de>
2024-06-21 21:22:41 +0200

    Update AceAssets.php

--- assets/AceAssets.php
+++ assets/AceAssets.php
@@ -2,13 +2,15 @@
 
 namespace humhub\modules\moduleEditor\assets;
 
-class AceAssets extends \yii\web\AssetBundle
+class AceAssets extends \humhub\components\assets\AssetBundle
 {
+    public $sourcePath = '@module-editor/resources';
+
     public $publishOptions = [
         'forceCopy' => false
     ];
-    
-    public $sourcePath = 'resources';
-    
-    public $js = ['js/ace.js'];
+
+    public $js = [
+        'js/ace.js',
+    ];
 }
