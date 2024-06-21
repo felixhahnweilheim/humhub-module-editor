93782eacd33b2d44bd0063a03ae2ff0799add6fe
Felix Hahn <info@hahn-felix.de>
2024-06-21 23:17:05 +0200

    Update index.php

--- views/editor/index.php
+++ views/editor/index.php
@@ -34,11 +34,8 @@ AceAssets::register($this);
 
 <?php $form = ActiveForm::begin(['id' => 'admin-index-form']); ?>
     <div class="form-group">
-        
+        <div id="editor"><?= htmlspecialchars($model->content) ?></div>
     </div>
-<div id="editor">
-	<?= $model->content ?>
-</div>
     <div class="form-group">
         <?= Html::submitButton(\Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
     </div>
