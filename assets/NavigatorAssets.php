34ba35d8d8c1a045b006330ee50198e110947c1f
Felix Hahn <info@hahn-felix.de>
2024-06-24 00:04:16 +0200

    Update NavigatorAssets.php

--- assets/NavigatorAssets.php
+++ assets/NavigatorAssets.php
@@ -2,7 +2,7 @@
 
 namespace humhub\modules\moduleEditor\assets;
 
-class AceAssets extends \humhub\components\assets\AssetBundle
+class NavigatorAssets extends \humhub\components\assets\AssetBundle
 {
     public $sourcePath = '@module-editor/resources/nav';
 
