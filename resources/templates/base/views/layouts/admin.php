42effcf12d39e8f01928701e28f8cfa1a3486c7c
Felix Hahn <info@hahn-felix.de>
2024-07-19 12:52:25 +0200

    update module template

--- resources/templates/base/views/layouts/admin.php
+++ resources/templates/base/views/layouts/admin.php
@@ -3,7 +3,7 @@
 $this->beginContent('@admin/views/layouts/main.php') ?>
 <div class="panel panel-default">
     <div class="panel-heading">
-        <?= Yii::t('{module_translation_base}.admin', '<strong>Edit Module</strong>'); ?>
+        <?= Yii::t('{module_translation_base}.admin', '{module_title}'); ?>
     </div>
 
     <?= \humhub\modules\{module_camelCase}\widgets\AdminMenu::widget(); ?>
